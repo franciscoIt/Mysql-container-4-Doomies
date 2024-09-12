e3148afb8b9f6d2d8efdbb0a048c09617847a297
franciscoIt <ivnro.dev@gmail.com>
2024-09-12 15:33:18 +0100

    working

--- script_docker_mysql.sh
+++ script_docker_mysql.sh
@@ -3,28 +3,28 @@
 PASSWORD=secret
 CONTAINER_NAME=mysql_container
 DB_NAME=mydb
+DOCKER_PORT=3306
+HOST_PORT=53306
 
 # Run the MySQL container in the background
 docker run -d --rm \
     --name=$CONTAINER_NAME \
     -e MYSQL_ROOT_PASSWORD=$PASSWORD \
     -e MYSQL_DATABASE=$DB_NAME \
-    -p 3307:3306 \
+    -p $HOST_PORT:$DOCKER_PORT \
     mysql:latest
 
 # Introduce a delay to allow MySQL to initialize
-sleep 20
-
-# Get the IP address of the running container
-DOCKER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME)
+echo "Charging..."
+sleep 5 
 
 # Optionally, you can check the MySQL logs for errors
 # docker logs $CONTAINER_NAME
 
 echo "Container started. Connecting to MySQL..."
 
-# Connect to MySQL using the Docker IP and mapped port
-mysql -u root -h $DOCKER_IP -P 3306 -p
+# Connect to MySQL using localhost and the mapped port
+mysql -u root -h 127.0.0.1 -P $HOST_PORT -p$PASSWORD
 
 # Stop the container
 docker stop $CONTAINER_NAME
